d0ab8656b9765a76c9c95951a8bd2627b758bde3
Ashley Martinez <ashley.carrasquel@gmail.com>
2017-10-18 02:55:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function theBeatlesPlay(musicians, instruments){
   var array = [];
   for(var m=0;m<musicians.length;m++){
-    var str = musicians[m] + 'plays' + instruments[m]; 
+    var str = musicians[m] + 'plays' + instruments[m];
+    array.push(str);
   }
 }
