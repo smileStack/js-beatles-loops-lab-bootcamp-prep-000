67b3ea22a1cc30c306707ae0f1105ff422792669
Ashley Martinez <ashley.carrasquel@gmail.com>
2017-10-18 02:59:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
 
 theBeatlesPlay(musicians,instruments);
 
@@ -11,3 +17,5 @@ function theBeatlesPlay(musicians, instruments){
   }
   return array
 }
+
+function johnLennonFacts()
